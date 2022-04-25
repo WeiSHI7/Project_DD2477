2f166daea2e3fb081a4e2b44a54fb8155722571c
Wei Shi <79168422+WeiSHI7@users.noreply.github.com>
2022-04-25 15:06:26 +0200

    updates

---
+++ load_json.py
@@ -0,0 +1,27 @@
+import json
+from elasticsearch import Elasticsearch, helpers
+
+
+filepath = "data\books_information_1.json"
+
+es = Elasticsearch()
+
+with open(filepath, "r") as filejson:
+    nodes = json.load(filejson)
+
+
+    # for node in nodes:
+    #     _id = node['bookid']
+    #     es.index(index='books',doc_type='external',id=_id,body=node)
+
+    actions = [
+    {
+    "_index" : "nodes_bulk",
+    "_type" : "external",
+    "_id" : str(node['index']),
+    "_source" : node
+    }
+    for node in nodes
+    ]
+
+helpers.bulk(es,actions)
