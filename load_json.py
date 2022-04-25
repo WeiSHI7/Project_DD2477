95f8ef0998bd0f75c61ed2620a1663c96e5f5fed
Wei Shi <79168422+WeiSHI7@users.noreply.github.com>
2022-04-25 15:51:06 +0200

    Update load_json.py

--- load_json.py
+++ load_json.py
@@ -15,7 +15,7 @@ def merge_json_files(dirpath):
         if filename.endswith(".json"):
             filename = dirpath + "/" + filename
             with open(filename, "r") as filejson:
-                nodes_in = json.loads(filejson)
+                nodes_in = json.load(filejson)
                 nodes = nodes | nodes_in
     return nodes
 
