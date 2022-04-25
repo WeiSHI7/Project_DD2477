83b868bbe790ed3d9987280becc16ff9d8820b13
Wei Shi <79168422+WeiSHI7@users.noreply.github.com>
2022-04-25 16:27:07 +0200

    Update load_json.py

--- load_json.py
+++ load_json.py
@@ -20,7 +20,7 @@ def merge_json_files(dirpath):
     return nodes
 
 
-filepath = "data/json_files_final/books_information_1.json"
+filepath = "data/json_files_final/books_information_2.json"
 
 es = Elasticsearch(scheme="http", timeout=150)
 
